8d0ae02cdf34655640eab5de012ca776ffdaee61
yuandasp <sitiyuandap@gmail.com>
2023-08-10 21:35:27 +0700

    create db-host

--- projects/server-pharmacy-app/src/database/index.js
+++ projects/server-pharmacy-app/src/database/index.js
@@ -2,7 +2,7 @@ const mysql = require("mysql2");
 const util = require("util");
 
 const db = mysql.createConnection({
-  host: "localhost",
+  host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
